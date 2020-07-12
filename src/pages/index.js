a4592482ce35b6111e8fb60c9e73ca32198a9cb8
Dillon Coffman <tug40394@temple.edu>
2020-07-12 12:02:22 -0400

    Got Google Sheet data and calculated total raised, total miles walked, and progress to goal

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,93 +5,118 @@ import BackgroundImage from "gatsby-background-image"
 import { graphql, Link as GatsbyLink } from "gatsby"
 import { Box, Progress, Text, SimpleGrid, PseudoBox } from "@chakra-ui/core"
 
-const IndexPage = ({ data }) => (
-  <Layout>
-    <SEO title="Home" />
-    <BackgroundImage
-      className="hero-container"
-      fluid={data.indexImage.childImageSharp.fluid}
-    >
-      <Box className="black-overlay">
-        <Box className="content-box" mx={[3, 5, "auto", "auto"]}>
-          <Box
-            as="h1"
-            className="content-box--hero-title"
-            textTransform="uppercase"
-            fontWeight="bold"
-            mb={3}
-          >
-            Walk Our Watershed
+const IndexPage = ({ data }) => {
+  const goal = 1000
+  const totalRaised = parseInt(
+    data.allGoogleFormResponses1Sheet.distinct[0],
+    10
+  )
+  let pageData = {}
+
+  const getTotalMiles = eventRegistrations => {
+    let sum = 0
+    eventRegistrations.forEach(eventR => {
+      sum += parseInt(eventR.miles, 10)
+    })
+    return sum
+  }
+
+  const getProgressToGoal = totalRaised => {
+    return (totalRaised / goal) * 100
+  }
+
+  pageData.totalMiles = getTotalMiles(data.allGoogleFormResponses1Sheet.nodes)
+  pageData.totalRaised = totalRaised
+  pageData.progressToGoal = getProgressToGoal(totalRaised)
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <BackgroundImage
+        className="hero-container"
+        fluid={data.indexImage.childImageSharp.fluid}
+      >
+        <Box className="black-overlay">
+          <Box className="content-box" mx={[3, 5, "auto", "auto"]}>
+            <Box
+              as="h1"
+              className="content-box--hero-title"
+              textTransform="uppercase"
+              fontWeight="bold"
+              mb={3}
+            >
+              Walk Our Watershed
+            </Box>
+            <Text
+              fontSize={["18px", "sm", "md"]}
+              textTransform="uppercase"
+              mb={3}
+            >
+              Take a pledge to walk, bike or paddle a combined 177 miles. 1 mile
+              for each stream mile in Jacobs Creek Watershed.
+            </Text>
+            <Text textTransform="uppercase">August 1st to September 30th</Text>
           </Box>
-          <Text
-            fontSize={["18px", "sm", "md"]}
-            textTransform="uppercase"
-            mb={3}
-          >
-            Take a pledge to walk, bike or paddle a combined 177 miles. 1 mile
-            for each stream mile in Jacobs Creek Watershed.
-          </Text>
-          <Text textTransform="uppercase">August 1st to September 30th</Text>
         </Box>
-      </Box>
-    </BackgroundImage>
-    <SimpleGrid columns={[1, null, 2]} spacing={0}>
-      <Box bg="teal.200" height="200px">
+      </BackgroundImage>
+      <SimpleGrid columns={[1, null, 2]} spacing={0}>
+        <Box bg="teal.200" height="200px">
+          <Box px={10} pt={2} textAlign="center">
+            <Text fontWeight="bold" color="white" fontSize={[30, 40]}>
+              Total Raised
+            </Text>
+            <Text fontWeight="bold" color="white" fontSize="70px">
+              ${pageData.totalRaised}
+            </Text>
+          </Box>
+        </Box>
+        <Box bg="teal.400" height="200px">
+          <Box px={10} pt={[5, 5, 5, 5]} fontSize={[30]} textAlign="center">
+            <Text pb={2} fontWeight="bold" color="white">
+              Progress to our $1,000 goal:
+            </Text>
+            <Progress
+              color="teal"
+              height={["50px", "60px", "60px", "100px"]}
+              value={pageData.progressToGoal}
+            />
+          </Box>
+        </Box>
+      </SimpleGrid>
+      <Box mt={5} bg="teal.200" height="200px">
         <Box px={10} pt={2} textAlign="center">
           <Text fontWeight="bold" color="white" fontSize={[30, 40]}>
-            Total Raised
+            Total Miles Covered
           </Text>
           <Text fontWeight="bold" color="white" fontSize="70px">
-            $505
+            {pageData.totalMiles}
           </Text>
         </Box>
       </Box>
-      <Box bg="teal.400" height="200px">
-        <Box px={10} pt={[5, 5, 5, 5]} fontSize={[30]} textAlign="center">
-          <Text pb={2} fontWeight="bold" color="white">
-            Progress to our $1,000 goal:
-          </Text>
-          <Progress
-            color="teal"
-            height={["50px", "60px", "60px", "100px"]}
-            value={60}
-          />
-        </Box>
+      <Box className="norwester" my={10} textAlign="center">
+        <PseudoBox
+          as={GatsbyLink}
+          to="/register"
+          backgroundColor="yellow.400"
+          borderRadius={5}
+          color="white"
+          padding={3}
+          px={10}
+          _hover={{ backgroundColor: "green.400", color: "gray.100" }}
+          _focus={{
+            outline: "none",
+            boxShadow: "outline",
+            borderRadius: 5,
+            backgroundColor: "green.400",
+            color: "gray.100",
+          }}
+        >
+          Register Here
+        </PseudoBox>
       </Box>
-    </SimpleGrid>
-    <Box mt={5} bg="teal.200" height="200px">
-      <Box px={10} pt={2} textAlign="center">
-        <Text fontWeight="bold" color="white" fontSize={[30, 40]}>
-          Total Miles Covered
-        </Text>
-        <Text fontWeight="bold" color="white" fontSize="70px">
-          1123
-        </Text>
-      </Box>
-    </Box>
-    <Box className="norwester" my={10} textAlign="center">
-      <PseudoBox
-        as={GatsbyLink}
-        to="/register"
-        backgroundColor="yellow.400"
-        borderRadius={5}
-        color="white"
-        padding={3}
-        px={10}
-        _hover={{ backgroundColor: "green.400", color: "gray.100" }}
-        _focus={{
-          outline: "none",
-          boxShadow: "outline",
-          borderRadius: 5,
-          backgroundColor: "green.400",
-          color: "gray.100",
-        }}
-      >
-        Register Here
-      </PseudoBox>
-    </Box>
-  </Layout>
-)
+    </Layout>
+  )
+}
 
 export const pageQuery = graphql`
   query {
@@ -101,6 +126,17 @@ export const pageQuery = graphql`
           ...GatsbyImageSharpFluid
         }
       }
+    }
+    allGoogleFormResponses1Sheet {
+      nodes {
+        gross
+        fullName
+        email
+        timestamp
+        date
+        miles
+      }
+      distinct(field: gross)
     }
   }
 `
