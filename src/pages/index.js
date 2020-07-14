d42ae794e221ce99b776f033059d2aafb5a8c160
Dillon Coffman <tug40394@temple.edu>
2020-07-14 19:48:28 -0400

    Updated home page with copy

--- src/pages/index.js
+++ src/pages/index.js
@@ -76,7 +76,11 @@ const IndexPage = ({ data }) => {
           </Box>
         </Box>
       </BackgroundImage>
-      <SimpleGrid columns={[1, null, 2]} spacing={0}>
+      <SimpleGrid
+        borderBottom="3px white solid"
+        columns={[1, null, 2]}
+        spacing={0}
+      >
         <Box bg="teal.200" height="200px">
           <Box px={[0, 4, 6, 10]} pt={2} textAlign="center">
             <Text fontWeight="bold" color="white" fontSize={[30, 40]}>
@@ -112,7 +116,7 @@ const IndexPage = ({ data }) => {
           </Box>
         </Box>
       </SimpleGrid>
-      <Box mt={5} bg="teal.200" height="200px">
+      <Box borderTop="3px white solid" bg="teal.200" height="200px">
         <Box px={[0, 4, 6, 10]} pt={2} textAlign="center">
           <Text fontWeight="bold" color="white" fontSize={[30, 40]}>
             Total Miles Covered
@@ -121,6 +125,51 @@ const IndexPage = ({ data }) => {
             {pageData.totalMiles}
           </Text>
         </Box>
+      </Box>
+      <Box
+        maxW={900}
+        mt={[5, 5, 10, 10]}
+        mx={[3, 5, 5, "auto"]}
+        fontSize={["18px", "sm", "md"]}
+      >
+        <Text mb={3}>
+          During these uncertain times of COVID-19 and social distancing, we
+          have kept moving forward and are still taking steps to uphold our
+          mission. Walk Our Watershed allows our community to virtually unite
+          and explore new ways to connect with one another and the remarkable
+          ecosystem we work to defend. Together we can take a pledge to focus on
+          healthy habits and a healthy watershed!
+        </Text>
+        <Text mb={3}>
+          From August 1 - September 30th, 2020, walkers, bikers and paddlers
+          from across multiple communities can take a pledge to cover a combined
+          177 miles, the total number of stream miles in Jacobs Creek Watershed.
+          Participants will raise awareness and vital funding for JCWA every
+          step of the way. Donate any amount of money for your pledge, even $1.
+          All participants will receive a JCWA magnet. Donations of $50 or more
+          will be rewarded with a Walk Our Watershed 2020 t-shirt after
+          September 30th! You will also receive weekly email updates of the
+          leaderboard to see who’s Walking our Watershed, a JCWA video with
+          place ideas to explore and some motivation to venture our watershed or
+          your own backyard.
+        </Text>
+        <Text mb={3}>
+          Jacobs Creek Watershed Association is a non-profit organization
+          dedicated to the conservation of our natural resources, rehabilitation
+          of the creek and watershed, environmental education and recreation.
+          For over 20 years we have been educating thousands of people,
+          resolving storm water issues, conserving our watershed and making it a
+          more beautiful and inviting place.
+        </Text>
+        <Text mb={3}>
+          With the support of people like{" "}
+          <Text as="span" display="inline" fontWeight="bold">
+            you
+          </Text>{" "}
+          who understand the value of the vital relationship between these
+          concepts, we will save our watershed and continue the vital work we do
+          for our local community.
+        </Text>
       </Box>
       <Box className="norwester" my={10} textAlign="center">
         <PseudoBox
